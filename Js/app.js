35e5a486a3e3e7641c5039b9f636a10d3dbdddc6
ahmed <ahmedsh717@gmail.com>
2020-06-01 14:15:16 +0300

    adding chart

--- Js/app.js
+++ Js/app.js
@@ -44,6 +44,8 @@ var products = [
     'water-can',
     'wine-glass'
 ];
+var totalClicks = [];
+var totalViews = [];
 var leftImg = document.getElementById('leftImg');
 var midImg = document.getElementById('midImg');
 var rightImg = document.getElementById('rightImg');
@@ -57,9 +59,10 @@ var random1 = 0,
     random3 = 0;
 
 var rounds = 0;
-var seassionLength = 25;
+var seassionLength = 10;
 var seassionEnd = false;
 Product.all = [];
+// the Constructor
 function Product(prodName, prodImgPath) {
     this.name = prodName;
     this.path = prodImgPath;
@@ -70,17 +73,19 @@ function Product(prodName, prodImgPath) {
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+// function to create object for each product
 function createObjects() {
     for (let i = 0; i < products.length; i++) {
         new Product(products[i], paths[i]);
     }
 }
-
+// function to give the images randome values
 function randomRender() {
     random1 = 0;
     random2 = 0;
     random3 = 0;
     while (random1 == random2 || random1 == random3 || random2 == random3) {
+        //if viewed before then change it
         random1 = randomNumber(0, products.length - 1);
         random2 = randomNumber(0, products.length - 1);
         random3 = randomNumber(0, products.length - 1);
@@ -106,6 +111,7 @@ function randomRender() {
     midProduct = Product.all[random2];
 
 }
+// function to create html tags
 function renderResult(){
     if (seassionEnd == false){
         var ul = document.getElementById('ulResult');
@@ -117,6 +123,42 @@ function renderResult(){
         seassionEnd = true;
     }
 }
+// function to fill tow arrais with data so we use it later in chart
+function fillDataSets(){
+for (let i = 0; i < Product.all.length; i++) {
+    totalClicks.push(Product.all[i].clicks);
+    totalViews.push(Product.all[i].views);
+}
+}
+// Render chart
+function renderChart(){
+    fillDataSets();
+    var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+        labels: products,
+        datasets: [{
+            label: 'Clicks',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: totalClicks
+        },{
+            label: 'Views',
+            backgroundColor: 'rgb(73, 197, 77)',
+            borderColor: 'rgb(73, 197, 77)',
+            data: totalViews
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+}
+
+// click handler function
 function handleClick(event) {
     if (rounds < seassionLength) {
         leftProduct.views += 1;
@@ -144,6 +186,7 @@ function handleClick(event) {
 
     } else {
         renderResult();
+        renderChart();
     }
 }
 
